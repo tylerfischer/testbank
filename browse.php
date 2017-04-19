1a61bfebbcf13ce6dd9c495700a4ad7fbe4e005d
tylerfischer <tylerfischer047@gmail.com>
2017-04-19 17:42:38 -0400

    re adding comments

--- browse.php
+++ browse.php
@@ -2,10 +2,10 @@
 <?php
 	session_start();
 	include_once "function.php";
-	
-	# If you are not logged in, Get out of here! # This needs to be changed to restrict some functionality. 
+
+	# If you are not logged in, Get out of here! # This needs to be changed to restrict some functionality.
 	if (!isset($_SESSION['loggedin'])) {
-		header("Location: login.php") ; 
+		header("Location: login.php") ;
 	}
 	unset($_GET['id']) ;
 ?>
@@ -14,21 +14,21 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Media Browse</title>
 
-<link rel="stylesheet" type="text/css" href="css/browse.css" /> 
+<link rel="stylesheet" type="text/css" href="css/browse.css" />
 
 <script type="text/javascript" src="js/jquery-latest.pack.js"></script>
 <script type="text/javascript">
-	
+
 function saveDownload(id)
 {
 	$.post("media_download_process.php",
 	{
        id: id,
 	},
-	function(message) 
+	function(message)
     { }
  	);
-} 
+}
 </script>
 </head>
 
@@ -40,208 +40,209 @@ function saveDownload(id)
 <nav class="navigation">
 					<button class="button" id="upload_file" onclick="upload_file()" >Upload File</button>
 					<button class="button" id="acct_btn">Edit Your (<?php echo $_SESSION['username']?>) Account</button>
-		
-		<form name="channelForm" id="channelForm" action="browse.php" method="post"> 
-			<input name="channelBtn" id="channel_btn" type="submit"  value="My Channel">	
+
+		<form name="channelForm" id="channelForm" action="browse.php" method="post">
+			<input name="channelBtn" id="channel_btn" type="submit"  value="My Channel">
 		</form>
-		
-		<form name="logoutForm" id="logoutForm" action="browse.php" method="post"> 
-			<input name="logoutBtn" id="log_out_btn" type="submit"  value="Log Out <?php echo $_SESSION['username'] ?> ">	
+
+		<form name="logoutForm" id="logoutForm" action="browse.php" method="post">
+			<input name="logoutBtn" id="log_out_btn" type="submit"  value="Log Out <?php echo $_SESSION['username'] ?> ">
 		</form>
-	
+		<a id="view_messages" href='messages.php'  style="color:#FF9900;">Inbox</a>
+
 </nav>
 
 
-<script> 
+<script>
 	function upload_file () {
-		console.info("Clicked Upload File Button ") ; 
-		window.location.replace("media_upload.php") ; 
+		console.info("Clicked Upload File Button ") ;
+		window.location.replace("media_upload.php") ;
 	}
-	
+
 
 </script>
-<!-- The following PHP code is used for taking the user to their channel.php -->  	
-		<?php 
+<!-- The following PHP code is used for taking the user to their channel.php -->
+		<?php
 			if(isset($_POST['channelBtn'])){
-				$mychannel = check_channel_exists(); 
-				header("Location: $mychannel"); 
+				$mychannel = check_channel_exists();
+				header("Location: $mychannel");
 			}
 		?>
 
-<!-- The following PHP code is used to Update Account Profile --> 
+<!-- The following PHP code is used to Update Account Profile -->
 
 
-<!-- The following PHP code is used for logging out the user and return to index.php -->  	
-		<?php 
+<!-- The following PHP code is used for logging out the user and return to index.php -->
+		<?php
 			if(isset($_POST['logoutBtn'])){
 				$_SESSION['username'] = NULL ;
-				$_SESSION['loggedin'] = NULL ; 
-				header("Location: index.php"); 
+				$_SESSION['loggedin'] = NULL ;
+				header("Location: index.php");
 			}
 		?>
-	
-	
 
-<!-- The following PHP code is used to Update Account Profile --> 
-<?php 
-	# The following is the prompt for Accessing / Updating Account Information 
-	$acct_info = get_user_info($_SESSION['username']) ;  
+
+
+<!-- The following PHP code is used to Update Account Profile -->
+<?php
+	# The following is the prompt for Accessing / Updating Account Information
+	$acct_info = get_user_info($_SESSION['username']) ;
 
 	if(isset($_POST['profile_name'])){
 		$post_name = $_POST['profile_name'] ;
-		update_profile($_POST['profile_name'], "name" ) ; 
+		update_profile($_POST['profile_name'], "name" ) ;
 		unset($_POST['profile_name']) ;
 	}
 	if(isset($_POST['pw'])){
 		$post_pass = $_POST['pw'] ;
-		update_profile($_POST['pw'], "pass" ) ; 
+		update_profile($_POST['pw'], "pass" ) ;
 		unset($_POST['pw']) ;
 	}
 	if(isset($_POST['emails'])){
 		$post_email = $_POST['emails'] ;
-		update_profile($_POST['emails'], "email" ) ; 
+		update_profile($_POST['emails'], "email" ) ;
 		unset($_POST['emails']) ;
 }
-?> 
+?>
 <div id="acct_prompt" class="prompt">
 
 	<div id="acct_prompt_content">
 		<span id="acct_prompt_close" class="close">&times;</span>
 		<h4>Account Settings</h4>
-		<!-- Account (Update) Settings Form --> 
-  		<form id="acct_form" method="post" action="browse.php" > 
-		
+		<!-- Account (Update) Settings Form -->
+  		<form id="acct_form" method="post" action="browse.php" >
+
 		Name:<input id="profile_name" name="profile_name" type="text" class="text" value="<?php echo $acct_info[1]?>" >
-        <br/> 
-        <br/> 
+        <br/>
+        <br/>
 		Password: <input id="pw" name="pw" type="password" class="text" value="<?php echo $acct_info[3]?>">
-       	<br/> 
-       	 <br/> 
+       	<br/>
+       	 <br/>
 		Email: <input id="emails" name="emails" type="text" class="text" value="<?php echo $acct_info[5]?>" >
-        <br/> <br/> 
+        <br/> <br/>
 	      	<input name="submit" type="submit" autofocus class="submit" value="Save">
   </form>
 
-	</div> 
+	</div>
 </div>
 <script>
 	var acct_prompt1 = document.getElementById('acct_prompt') ;
-	var acct_prompt_button = document.getElementById('acct_btn') ; 
-	var acct_prompt_close1 = document.getElementById('acct_prompt_close'); 
+	var acct_prompt_button = document.getElementById('acct_btn') ;
+	var acct_prompt_close1 = document.getElementById('acct_prompt_close');
 
 	acct_prompt_button.onclick = function(){
-		console.info("Clicked Acct Button") ; 
-		acct_prompt1.style.display = "block" ; 
+		console.info("Clicked Acct Button") ;
+		acct_prompt1.style.display = "block" ;
 	}
 	acct_prompt_close1.onclick = function() {
-		console.info("Clicked Close Button") ; 
-		acct_prompt1.style.display = "none" ; 
+		console.info("Clicked Close Button") ;
+		acct_prompt1.style.display = "none" ;
 	}
 	window.onclick = function(event){
-		if ( event.target == acct_prompt1 ) 
-			acct_prompt1.style.display = "none" ; 
+		if ( event.target == acct_prompt1 )
+			acct_prompt1.style.display = "none" ;
 	}
-	
+
 </script>
-<!-- _____________ END of Prompt Section ______________________________________ --> 
+<!-- _____________ END of Prompt Section ______________________________________ -->
 
 
 <div id='upload_result'>
-<?php 
+<?php
 	if(isset($_REQUEST['result']) && $_REQUEST['result']!=0)
-	{		
+	{
 		echo upload_error($_REQUEST['result']);
 	}
 ?>
 </div>
 <br/><br/>
-<?php # Loads all the media available. 
-	$result = load_media() ; 
+<?php # Loads all the media available.
+	$result = load_media() ;
 ?>
 	<h2 id="category_header"> Browse by Category </h2>
-	<div id="cat_div"> 
-	<?php 
-		$categories = categorized_media( ) ; 
-		while ($result_row = mysqli_fetch_row($categories)) 
-			{ 
-				$category = $result_row[4]; 
-				
-		?> <form class="cat_form" method="get" action="browse.php"> 
-		<input id="cat_input" onClick="cat() ;"  name="category" type="submit" class="button" value="<?php echo $category ?>" /> 
+	<div id="cat_div">
+	<?php
+		$categories = categorized_media( ) ;
+		while ($result_row = mysqli_fetch_row($categories))
+			{
+				$category = $result_row[4];
+
+		?> <form class="cat_form" method="get" action="browse.php">
+		<input id="cat_input" onClick="cat() ;"  name="category" type="submit" class="button" value="<?php echo $category ?>" />
 		 <?php
 			}
-		?> 	
+		?>
 		</form>
-	<script> 
+	<script>
 		function cat (){
-			console.info("Clicked a Cat button ") ; 
-			<?php 
+			console.info("Clicked a Cat button ") ;
+			<?php
 			if ( isset($_GET['category'])) {
-				unset($_GET['keyword']); 
+				unset($_GET['keyword']);
 				//echo "Hello there "  ;
-			 $result = cat_media ($_GET['category'] ) ; 
+			 $result = cat_media ($_GET['category'] ) ;
 		}
 	?>
-	}	
-		
-	</script> 
-	</div> 
+	}
+
+	</script>
+	</div>
 	<h2 id="search_header">Search by Keyword</h2>
-	
-  	<div id="keyword_div"> 
-  
-    <form id="search_form" method="get" action="browse.php" > 
+
+  	<div id="keyword_div">
+
+    <form id="search_form" method="get" action="browse.php" >
     	<input class="text" name="keyword" type="text" id="search_box">
-    	<input type="submit" > 
+    	<input type="submit" >
 	</form>
-	<?php 
+	<?php
 		if ( isset($_GET['keyword'])) {
-			$result = find_media ($_GET['keyword'] ) ; 
+			$result = find_media ($_GET['keyword'] ) ;
 		}
 	?>
-	
-	<button id="clear_btn"> Clear Filter </button>	 
-    <script> 
-		var clear_button = document.getElementById('clear_btn') ; 
+
+	<button id="clear_btn"> Clear Filter </button>
+    <script>
+		var clear_button = document.getElementById('clear_btn') ;
 		clear_button.onclick = function(){
-		// Redirects the page back to the default browse.php page. 
+		// Redirects the page back to the default browse.php page.
 		window.location.replace("browse.php") ;
-	}	
-	</script> 
+	}
+	</script>
   </div>
-   
+
     <h2 id="table_header">All Media</h2>
     <table id="top_row" width="50%" cellpadding="5" cellspacing="10">
 	<tr valign="top">
 	<td>Media ID</td><td>View File </td><td>Download file</td>
 	</tr>
-	
+
 	</table>
 	<table width="50%" cellpadding="5" cellspacing="10" border="2" bordercolor="#895803">
 		<?php
 			while ($result_row = mysqli_fetch_row($result)) //filename, username, type, mediaid, path
-			{ 
+			{
 				$mediaid = $result_row[0]; // was 3
-				$filename = $result_row[1]; // was 0 
-				$filenpath = $result_row[2]; // was 4   // This seems correct! 
-		?> 
-        	 <tr valign="top">			
+				$filename = $result_row[1]; // was 0
+				$filenpath = $result_row[2]; // was 4   // This seems correct!
+		?>
+        	 <tr valign="top">
 			<td>
-					<?php 
-						echo $mediaid ; 
+					<?php
+						echo $mediaid ;
 					?>
 			</td>
                       <td>
-           	            <script> 
+           	            <script>
 							function view_media(id) {
-								console.info("The id is " + id ) ; 
-							return ; 
+								console.info("The id is " + id ) ;
+							return ;
 							}
-						</script>            	            
+						</script>
             	            <a href="media.php?id=<?php echo $mediaid;?>"><?php echo $filename;?></a>
                         </td>
                         <td>
-            	            <a href="<?php echo $filenpath;?>" target="_blank" onclick="javascript:saveDownload(<?php echo $result_row[2];?>);" download> Download</a> 
+            	            <a href="<?php echo $filenpath;?>" target="_blank" onclick="javascript:saveDownload(<?php echo $result_row[2];?>);" download> Download</a>
                         </td>
 		</tr>
         	<?php
@@ -249,7 +250,7 @@ function saveDownload(id)
 		?>
 	</table>
    </div>
-   
-	 
+
+
 </body>
 </html>
--- media.php
+++ media.php
@@ -3,54 +3,54 @@
 	session_start();
 	include_once "function.php";
 
-	# If you are not logged in, Get out of here! 
+	# If you are not logged in, Get out of here!
 	if (!isset($_SESSION['loggedin'])) {
-		header("Location: login.php") ; 
+		header("Location: login.php") ;
 	}
-?>	
+?>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Media</title>
 <script src="Scripts/AC_ActiveX.js" type="text/javascript"></script>
-<link rel="stylesheet" type="text/css" href="css/media.css" /> 
+<link rel="stylesheet" type="text/css" href="css/media.css" />
 <script src="Scripts/AC_RunActiveContent.js" type="text/javascript"></script>
 </head>
 
 <body>
- 
+
 <?php
 
 if(isset($_GET['id'])) {
-	# echo "This is GET id :" . $_GET['id'] ; 
-	$id = $_GET['id'] ; 
+	# echo "This is GET id :" . $_GET['id'] ;
+	$id = $_GET['id'] ;
 	$query = "SELECT * FROM Media WHERE Media_ID = $id  ";
 	$result = mysqli_query($GLOBALS['con'],  $query );
 	$result_row = mysqli_fetch_row($result);
-	
-	//updateMediaTime($_GET['id']); //  or $id 
-	
+
+	//updateMediaTime($_GET['id']); //  or $id
+
 	$filename=$result_row[1];   ////0, 4, 2
-	$filepath=$result_row[2]; 
+	$filepath=$result_row[2];
 	$type=$result_row[3];
 	if(substr($type,0,5)=="image") //view image
 	{
 		echo "Viewing Picture:";
-		echo $result_row[2]; // was 4 
-		
+		echo $result_row[2]; // was 4
+
 		echo "<img src='".$filepath."'/>";
 	}
 	else //view movie
-	{	
+	{
 ?>
 	<!-- <p>Viewing Video:<?php echo $result_row[3].$result_row[1];?></p> -->
-	<p>Viewing Video:<?php echo $result_row[2];?></p> <!-- was 4 --> 
-	      
+	<p>Viewing Video:<?php echo $result_row[2];?></p> <!-- was 4 -->
+
     <object id="MediaPlayer" width=320 height=286 classid="CLSID:22D6f312-B0F6-11D0-94AB-0080C74C7E95" standby="Loading Windows Media Player components…" type="application/x-oleobject" codebase="http://activex.microsoft.com/activex/controls/mplayer/en/nsmp2inf.cab#Version=6,4,7,1112">
 
-<param name="filename" value="<?php echo $result_row[2];?>"> <!-- was 4 --> 
+<param name="filename" value="<?php echo $result_row[2];?>"> <!-- was 4 -->
 	<!-- echo $result_row[2].$result_row[1];  -->
-		
+
 
 <param name="Showcontrols" value="True">
 <param name="autoStart" value="True">
@@ -69,5 +69,63 @@ else
 <?php
 }
 ?>
+<!-- ____the following code is used to post comments on a video___ -->
+<form action="" method ="post">
+	Comment : <textarea name="comment" rows="6" cols="50"></textarea>
+	<input type="submit" name="submit">
+</form>
+<?php
+if(isset($_POST["submit"]))
+{
+$comment = $_POST["comment"];
+
+$username = $_SESSION['username'];
+
+$insert1 = "INSERT INTO `Comment`(`Comment_ID`,`Username` , `Comment`, `Media_ID`, `TimeSt`) VALUES (DEFAULT, '$username' , '$comment', '$id', DEFAULT);";
+$queryresult = mysqli_query($GLOBALS['con'] , $insert1)
+		or die("Insert into Media error in media_upload_process.php ". $insert1 . "  ". mysql_error());
+$result="0";
+echo '<center> Comment Successfully Submitted </center>';
+
+}
+
+ ?>
+
+ <?php # Loads all the comments for a given video.
+ 	$result = load_comments($id) ;
+ ?>
+
+     <h4>Comments</h4>
+     <table id="top_row" width="50%" cellpadding="5" cellspacing="10">
+ 	<tr valign="top">
+ 	<td>User</td><td>Comment</td><td>timestamp</td>
+ 	</tr>
+
+ 	</table>
+ 	<table width="50%" cellpadding="5" cellspacing="10" border="2" bordercolor="#895803">
+ 		<?php
+ 			while ($result_row = mysqli_fetch_row($result)) //filename, username, type, mediaid, path
+ 			{
+ 				$username = $result_row[0]; // was 3
+ 				$comment = $result_row[1]; // was 0
+ 				$time = $result_row[2]; // was 4   // This seems correct!
+ 		?>
+     <tr valign="top">
+ 					<td>
+						<button class="button" id="msg_btn"><?php echo $username; ?></button>
+ 					</td>
+ 		      <td>
+ 		        <?php echo $comment;?>
+					</td>
+					<td>
+						<?php echo $time;?>
+ 		      </td>
+ 		</tr>
+     <?php
+ 			}
+ 		?>
+ 	</table>
+    </div>
+
 </body>
 </html>
