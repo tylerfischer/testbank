d7864311171dff7785364a623a8d6c99af7d3cf3
tylerfischer <tylerfischer047@gmail.com>
2017-04-19 11:59:48 -0400

    Test

--- media.php
+++ media.php
@@ -43,7 +43,8 @@ if(isset($_GET['id'])) {
 	else //view movie
 	{
 ?>
-	<!-- <p>Viewing Video:<?php echo $result_row[3].$result_row[1];?></p> -->
+	<!-- <p>Viewing Video:
+	<?php echo $result_row[3].$result_row[1];?></p> -->
 	<p>Viewing Video:<?php echo $result_row[2];?></p> <!-- was 4 -->
 
     <object id="MediaPlayer" width=320 height=286 classid="CLSID:22D6f312-B0F6-11D0-94AB-0080C74C7E95" standby="Loading Windows Media Player components…" type="application/x-oleobject" codebase="http://activex.microsoft.com/activex/controls/mplayer/en/nsmp2inf.cab#Version=6,4,7,1112">
@@ -69,6 +70,8 @@ else
 <?php
 }
 ?>
+
+<h1>WHAT IS THIS</h1>
 <form action="" method ="post">
 	Comment : <textarea name="comment" rows="6" cols="50"></textarea>
 	<input type="submit" name="submit">
